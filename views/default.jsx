5997f55844305258ffb66d5bef77b20b04e384c3
Ryan Wendell <Ryan.wendell@me.com>
2022-08-31 15:01:43 -0700

    Added meta tags

--- views/default.jsx
+++ views/default.jsx
@@ -2,10 +2,14 @@ const React = require('react')
 
 function Def (html){
     return(
-        <html>
+        <html lang='en'>
             <head>
                 <meta charset="UTF-8"/>
+                <meta name="description" content="Chess-like games"/>
+                <meta name="keywords" content="Chess, Chess-like, Latrunculi, Tafl, Tablut, Brandubh, Tawlbwrdd"/>
+                <meta name="author" content="Ryan Wendell"/>
                 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+                
                 {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
                 <meta name="viewport" content="width=1000"></meta>
                 <title>Chess-like</title>
--- views/game.jsx
+++ views/game.jsx
@@ -80,11 +80,13 @@ function game (data) {
         )
     }
     return(
-        <html>
+        <html lang='en'>
             <head>
                 <meta charset="UTF-8"/>
                 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-                {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
+                <meta name="description" content="Chess-like games"/>
+                <meta name="keywords" content="Chess, Chess-like, Latrunculi, Tafl, Tablut, Brandubh, Tawlbwrdd"/>
+                <meta name="author" content="Ryan Wendell"/>
                 <meta name="viewport" content="width=1000"></meta>
                 <title>{data.name}</title>
                 <link rel="stylesheet" href="/style.css"/>
