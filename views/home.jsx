a6705f82159acadb71284cb9e6be580ea10d5979
Ryan Wendell <Ryan.wendell@me.com>
2022-09-02 16:24:49 -0700

    removed usless file

--- views/home.jsx
+++ views/home.jsx
@@ -1,5 +1,4 @@
 const React = require('react')
-const Def = require('./default')
 
 let buttons = (data) =>{
     return(
@@ -48,17 +47,30 @@ let buttons = (data) =>{
 
 function home (data) {
     return (
-        <Def>
-            <main>
-                <div id='Header'>
-                    <div id='WebsiteInfo'>
-                        <h1>Chess-Like Games</h1>
-                        <p>Welcome, here are a couple of games that are like Chess, including Chess.</p>
+        <html lang='en'>
+            <head>
+                <meta charset="UTF-8"/>
+                <meta name="description" content="Chess-like games"/>
+                <meta name="keywords" content="Chess, Chess-like, Latrunculi, Tafl, Tablut, Brandubh, Tawlbwrdd"/>
+                <meta name="author" content="Ryan Wendell"/>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+                <meta name="viewport" content="width=1000"></meta>
+                <title>Chess-like</title>
+                <link rel="stylesheet" href="/style.css"/>
+            </head>
+            <body>
+                <main>
+                    <div id='Header'>
+                        <div id='WebsiteInfo'>
+                            <h1>Chess-Like Games</h1>
+                            <p>Welcome, here are a couple of games that are like Chess, including Chess.</p>
+                        </div>
                     </div>
-                </div>
-                {buttons(data)}
-            </main>
-        </Def>
+                    {buttons(data)}
+                </main>
+
+            </body>
+        </html>
     )
 }
 module.exports = home
