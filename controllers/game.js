5ea30b63859bb8a69ef0037e131dbe1c55831abd
Ryan Wendell <Ryan.wendell@me.com>
2022-09-11 17:20:06 -0700

    changed route from /:id to /:name

--- controllers/game.js
+++ controllers/game.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const games = require('../models/games').games
 const db = require('../models')
 
-router.get('/:id', (req, res) => {
-    db.Game.findById(req.params.id)
+router.get('/:name', (req, res) => {
+    db.Game.findOne({name: req.params.name})
     .then(game => {
         res.render('game', game)
     })
